27c608c5d3b072b6d3ddb6cdef13d3867ad6bc32
easyonez <aala@ajmai.me>
2026-02-13 00:29:16 +0100

    Fix case sensitivity in extractors and update .gitignore

--- app.php
+++ app.php
@@ -6,14 +6,14 @@ use AjUnzip\Unzipper;
 use AjUnzip\Extractors\ZipExtractor;
 use AjUnzip\Extractors\GzExtractor;
 use AjUnzip\Extractors\TarExtractor;
-use AjUnzip\Extractors\SevenzipExtractor;
+use AjUnzip\Extractors\SevenZipExtractor;
 
 $app = new Unzipper();
 
 $app->registerExtractor('zip', new ZipExtractor());
 $app->registerExtractor('gz', new GzExtractor());
 $app->registerExtractor('tar', new TarExtractor());
-$app->registerExtractor('7z', new SevenzipExtractor());
+$app->registerExtractor('7z', new SevenZipExtractor());
 
 $app->run($argv[1], './output');
 ?>
